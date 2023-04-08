ee3c36224dd30943a49f50e51b6d8a211f3c02af
Vaschell Mkenzie <mvaschell@gmail.com>
2023-04-07 22:30:45 -0400

    under the sea

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const scuberDivers = [`Antonia`,`Nuru`, `Amari`,`Mo`]
+
+const returnFirstTwoDrivers = function(scuberDivers){
+    return scuberDivers.slice(0, 2)
+}
+const returnLastTwoDrivers = function(){
+    return scuberDivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(num1){
+    return function(fare){
+        return fare * num1
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(scuberDivers, fn) {
+    return fn(scuberDivers);
+}
